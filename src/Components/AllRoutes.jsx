2c31732d154ca141c907dea3f50ba83b152b069b
sakti8093 <saktiprasad80932gmail.com>
2022-10-14 22:03:47 +0530

    added login and sign up

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -4,14 +4,23 @@ import { Collections } from "../Pages/Collections";
 import { Home } from "../Pages/Home";
 import { IndivisualCategory } from "../Pages/IndivisualCategory";
 import { SailWithBoat } from "../Pages/SailWithBoat";
+import ChangePassword from "../Pages/ChangePassword";
+import ForgotPassword from "../Pages/ForgotPassword";
+import LoginPage from "../Pages/Login";
+import SignUpPage from "../Pages/signup"
 
 export const AllRoutes = () => {
   return(
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path='/signup' element={<SignUpPage/>} ></Route>
+      <Route path="/login" element={<LoginPage/>} ></Route>
+      <Route path='/change_password' element={<ChangePassword/>} ></Route>
+      <Route path="/forgot_password" element={<ForgotPassword/>} ></Route>
       <Route path="/collections/:id/" element={<Collections />}></Route>
       <Route path="/collections/sail-with-boat" element={<SailWithBoat />}></Route>
       <Route path="/collections/:id/:url" element={<IndivisualCategory />}></Route>
+
     </Routes>
   );
 };
